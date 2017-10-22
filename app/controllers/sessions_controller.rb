0c254c6003c6f404894c876723841038f7ba4c46
John L. Cao <john_caohaha@hotmail.com>
2017-10-22 03:37:14 -0700

    recommended connections page styles round 1 and misc improvements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,6 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(name: params[:user][:name])
   	password = params[:user][:password]
-    puts 
-    puts password
-    puts params[:user][:name]
-    puts
   	if user && (user.authenticate("#{password}") != false)
   		session[:user_id] = user.id
   		redirect_to root_path, notice: "Logged in succesfully"
